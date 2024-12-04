242372480725a47d92158ce96f65c6ca3a8da30e
Augustine Chibueze Chibueze <157700327+austinLorenzMccoy@users.noreply.github.com>
2024-12-04 06:48:02 +0100

    application.py

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
 
-# Import your prediction pipeline
+# Import prediction pipeline
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 application = Flask(__name__)
